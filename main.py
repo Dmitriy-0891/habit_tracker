817b2d1df686d47238f4ed7ab3578cff3adfc0a9
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-04-25 00:22:48 +0300

    change main.py, added logging.info

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from pathlib import Path
 from datetime import datetime
 
 # Натсраиваю логирование (вместо print())
-logging.basicConfig(level=loging.INFO, format='%(asctime)s - %(massage)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', force=True)
 
 data_file = Path("habits.txt")
 
 def add_habbit(name: str):
     """Добавляет привычку в файл"""
+    logging.info(f"Добавлена привычка: {name}") 
     with open(data_file, 'a') as  f:
         f.write(f'{name}, {datetime.now()}\n')
         
-if __name__ = '__main__':
+if __name__ == '__main__':
     add_habbit('прочитать 10 страниц')
+    print('программа завершила работу')
