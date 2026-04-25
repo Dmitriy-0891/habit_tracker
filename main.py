3992534d7f498275040df7f93d2968434027a03f
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-04-25 23:52:09 +0300

    update main.py: added ability to output 2 habits

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', forc
 
 data_file = Path("habits.txt")
 
-def add_habbit(name: str):
+def add_habbit(name: str, name_2: str):
     """Добавляет привычку в файл"""
-    logging.info(f"Добавлена привычка: {name}") 
+    logging.info(f"Добавлена привычка: {name}\n Добавлена привычка: {name_2}") 
     with open(data_file, 'a') as  f:
         f.write(f'{name}, {datetime.now()}\n')
+    
+    with open(data_file, 'a') as f:
+        f.write(f'{name_2}, {datetime.now()}\n')
         
 if __name__ == '__main__':
-    add_habbit('прочитать 10 страниц')
+    add_habbit('прочитать 10 страниц', 'изучить слепую печать на qwerty-клавиатуре')
     print('программа завершила работу')
